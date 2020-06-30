6dcc3ea984b6e94ef8a2308ca66a86a0354db5d7
sagonzalezp <ser55.rock@gmail.com>
2020-06-30 12:55:23 -0500

    Agregando before y after a search-test.js

--- cypress/integration/search-test.js
+++ cypress/integration/search-test.js
@@ -1,10 +1,24 @@
 /// <reference types="cypress" />
 
 describe('Search test cases', ()=>{
-    it('Search results', ()=>{
+    before(()=>{
+        cy.log("Ejecutando precondiciones de las pruebas");
+    })
+
+    after(()=>{
+        cy.log("Ejecutando todas las poscondiciones");
+    })
+
+    it('Search dresses', ()=>{
         cy.visit("http://automationpractice.com/");
         cy.get('#search_query_top').type('dress');
         cy.get('#searchbox > .btn').click();
         cy.get('.lighter').contains('"dress"');
+    })
+    it('Search hats', ()=>{
+        cy.visit("http://automationpractice.com/");
+        cy.get('#search_query_top').type('hat');
+        cy.get('#searchbox > .btn').click();
+        cy.get('.lighter').contains('"hat"');
     })
 })
