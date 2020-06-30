909a61ae330fcb7c47db1de95b11e6ec4d999bcf
sagonzalezp <ser55.rock@gmail.com>
2020-06-30 16:56:06 -0500

    Como acceder a los elementos de una lista desplegable

--- cypress/integration/womenTest.js
+++ cypress/integration/womenTest.js
@@ -15,4 +15,26 @@ describe('Pruebas en el sitio Women', ()=>{
        cy.get('#layered_id_attribute_group_3').uncheck();
        cy.get('#layered_category_4').uncheck();
     })
+
+    it('Order by Lowest first', ()=>{
+        cy.get('#selectProductSort').select('Price: Lowest first');
+    })
+    it('Order by Higest Price', ()=>{
+        cy.get('#selectProductSort').select('Price: Highest first');
+    })
+    it('Order by Name: A to Z', ()=>{
+        cy.get('#selectProductSort').select('Product Name: A to Z');
+    })
+    it('Order by Name: Z to A', ()=>{
+        cy.get('#selectProductSort').select('Product Name: Z to A');
+    })
+    it('Order by In stock', ()=>{
+        cy.get('#selectProductSort').select('In stock');
+    })
+    it('Order by Reference: Lowest first', ()=>{
+        cy.get('#selectProductSort').select('Reference: Lowest first');
+    })
+    it('Order by Reference: Lowest first', ()=>{
+        cy.get('#selectProductSort').select('Reference: Highest first');
+    })
 })
