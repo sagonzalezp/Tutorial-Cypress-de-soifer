a75e3f941bb349f9091a7981f89efb8fab560d4d
sagonzalezp <ser55.rock@gmail.com>
2020-07-01 10:28:44 -0500

    Seleccionando cualquier elemento de una lista

--- cypress/integration/goToSections.js
+++ cypress/integration/goToSections.js
@@ -1,8 +1,13 @@
 /// <reference types="cypress" />
 
 describe('Go to diferent section', ()=>{
-    it('go to Woman section', ()=>{
+    beforeEach(()=>{
         cy.visit('http://automationpractice.com/index.php');
+    })
+    it('go to Woman section', ()=>{
         cy.get('[class=sf-with-ul]').first().click();
+    })
+    it('Go to Dresses section', ()=>{
+        cy.get('[class=sf-with-ul]').eq(3).click();
     })
 })
