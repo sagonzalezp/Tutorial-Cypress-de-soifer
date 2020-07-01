02e1f6e452bb7b56ce16d7f3b7c23f3c3f23f0ed
sagonzalezp <ser55.rock@gmail.com>
2020-07-01 11:11:48 -0500

    agregando comentarios de error a los asserts

--- cypress/integration/pruebaCuentas.js
+++ cypress/integration/pruebaCuentas.js
@@ -3,13 +3,13 @@
 describe('Cuentas', ()=>{
     let a = 1;
     it('probar una igualdad', ()=>{
-        expect(1==1).to.equal(true); //evalúa que lo que está dentro del expect() sea igual a lo que está dentro del equal()
+        expect(1==2, 'ambos términos tienen que ser iguales y no lo son').to.equal(true); //expect(prueba a evaluar, 'comentario en caso de error')
     })
     it('probar que no sea una igualdad', ()=>{
         expect(1==2).to.equal(false);
     })
     it('probar una resta', ()=>{
-        expect(1-1).to.equal(0);
+        expect(1-1, 'Aquí se espera un cero').to.equal(0);
     })
     it('probar que una resta no sea correcta', ()=>{
         expect(1-1).to.not.equal(1);
@@ -22,13 +22,13 @@ describe('Cuentas', ()=>{
     it('probar una condición falsa', ()=>{
         expect(1==2).to.be.false;
     })
-    it('probar que una variable dada, existe'){
+    it('probar que una variable dada, existe', ()=>{
         expect(a).to.exist;
-    }
+    })
     it('Es menor a 10', ()=>{
         expect(a).to.be.lessThan(10);
     })
     it('Es mayo a 10', ()=>{
         expect(25).to.be.greaterThan(10);
     })
-}
+})
